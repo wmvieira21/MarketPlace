bd421602c10bd6ac244fbc81570ae642c00a92d8
wmvieira-philips <william.vieira@philips.com>
2023-06-06 17:08:32 -0300

    mama

--- js/events.js
+++ js/events.js
@@ -3,11 +3,18 @@ import * as product from "./products.js";
 
 export default function loadEvents() {
   init.loadDynamicComponent(".product-button__buy").forEach((button) => {
+    
     button.addEventListener("click", (event) => {
       document.querySelector(".main-cart__cartSVG").dataset.cart =
         ++document.querySelector(".main-cart__cartSVG").dataset.cart;
-      
-        console.log(product.getProductById(parseInt(event.target.value)));
+
+      console.log(product.getProductById(parseInt(event.target.value)));
     });
   });
+
+  init.backdrop.addEventListener('click', (event) => {
+    init.backdrop.style.display = 'none';
+    console.dir(init.backdrop);
+  });
+
 }
--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
 export const productsSection = document.querySelector(".products-section");
+export const backdrop = document.querySelector(".backdrop");
+
 
 export const loadDynamicComponent = (componentClass) => {
   return document.querySelectorAll(componentClass);
